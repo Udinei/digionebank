af627ed30df915a167e8b7bc39773fcc0c0962a9
Udinei <ukaliko@gmail.com>
2021-08-21 21:04:43 -0400

    Criação de atributos

--- src/main/kotlin/one/digitalInnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalInnovation/digionebank/Pessoa.kt
@@ -1,6 +1,14 @@
 package one.digitalInnovation.digionebank
 
 class Pessoa {
+  var nome: String = "Jether"
+    var cpf: String = "123.123.123-15"
 
 
+}
+
+fun main(){
+    val jether = Pessoa()
+    println(jether.nome)
+    println(jether.cpf)
 }
